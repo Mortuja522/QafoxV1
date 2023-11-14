fa0bc3ecf4a5249a5e86fd73650200517583c5bf
HP <mortujaaiub@gmail.com>
2023-11-14 22:57:11 +0600

    Web-Automation-Testing

---
+++ src/main/java/QafoxV1/QafoxV1/App.java
@@ -0,0 +1,13 @@
+package QafoxV1.QafoxV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
